40acd2632aae82ab38538a7916904366329d7655
drelbert <danrelbert@gmail.com>
2019-08-21 18:13:21 -0400

    ng deeps dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MediaObserver } from '@angular/flex-layout';
 
-@Component({
+@Component({  // Decorator that defines the class below as a component
   selector: 'ui-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+// Below the app logic defined, support the view by inserting the title text
+// parts, property is title and value is 'ngSandbox'
 export class AppComponent {
   title = 'ngSandbox';
 
@@ -15,3 +17,6 @@ export class AppComponent {
 
 }
 
+// Note that the component is created, updated and destroyed with lifecucle hooks
+// Hook example = ngOnInit() for initialize the component or directive after Angular displays the data-bound properties
+// Hook example = ngOnChanges() responds when Angular sets or resets data-bound inoput properties
